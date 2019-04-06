70d81a56988ba546ebf931c7b5878d2353de2df3
bryanjay28 <bjay28@hotmail.com>
2019-04-06 15:37:15 -0400

    camera start

--- frontend/App.js
+++ frontend/App.js
@@ -1,15 +1,45 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Expo, Permissions, ImagePicker } from 'expo';
+
+import CameraScreen from './components/Camera';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    state = {
+      welcomeScreen: true,
+      hasCameraPermission: null,
+    }
+  }
+
+  componentWillMount = async () => {
+    const cameraResult = await Permissions.askAsync(Permissions.CAMERA);
+    const cameraRollResult = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    this.setState({ hasCameraPermission: cameraResult.status === 'granted' && cameraRollResult.status === 'granted' });
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    const { welcomeScreen } = this.state;
+    if (welcomeScreen) {
+      return (
+        <View style={styles.container}>
+          <Text>Click to Enter the camera</Text>
+          <Button
+            title="Click to Enter the App"
+            onPress={this.setState({ welcomeScreen: !welcomeScreen })} />
+        </View>
+      );
+    } else {
+      return (
+        <View>
+          <CameraScreen />
+        </View>
+      );
+    }
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
---
+++ frontend/components/Camera.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Camera from 'react-native-camera';
+
+export default class Camera extends React.Component {
+    state = {
+
+    }
+
+    render() {
+        <View>
+            <Text>HELLO</Text>
+            <Camera
+                ref={(cam) => {
+                    this.camera = cam;
+                }}>
+
+            </Camera>
+        </View>
+    }
+} 
